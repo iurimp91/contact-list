6145f091ec16ca762065c3995acb0a0395567335
Iuri Magnago <iurimp91@gmail.com>
2021-12-03 11:47:56 -0300

    feat: contact card base

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import { AppBar, Typography } from "@mui/material";
+import { AppBar, Typography, Box, Container } from "@mui/material";
+import { ContactPage } from "@mui/icons-material";
+
+function ContactCard(): JSX.Element {
+  return (
+    <Box
+      sx={{
+        height: "50px",
+        border: "1px solid #6AEFAB",
+        borderRadius: "10px",
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <ContactPage sx={{ fontSize: "40px" }} />
+      <Typography sx={{ fontSize: "23px", ml: "15px" }}>
+        Iuri Magnago Pinto
+      </Typography>
+    </Box>
+  );
+}
 
 function App(): JSX.Element {
   return (
-    <AppBar sx={{ padding: "10px" }}>
-      <Typography variant="h4" textAlign="center">
-        CONTACT LIST
-      </Typography>
-    </AppBar>
+    <>
+      <AppBar position="static" sx={{ padding: "10px" }}>
+        <Typography variant="h4" textAlign="center">
+          CONTACT LIST
+        </Typography>
+      </AppBar>
+      <Container sx={{ mt: "20px" }}>
+        <ContactCard />
+      </Container>
+    </>
   );
 }
 
