64cf09f4cb39492058f4ba7e0cc0f20964798c28
Iuri Magnago <iurimp91@gmail.com>
2021-12-06 14:17:23 -0300

    feat: add responsive date picker

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,12 +1,15 @@
 /* eslint-disable no-console */
 import { Stack, TextField, Button } from "@mui/material";
 import { SaveOutlined } from "@mui/icons-material";
+import { DatePicker, LocalizationProvider } from "@mui/lab";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+
 import React, { useState } from "react";
 
 export default function Form(): JSX.Element {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [birthday, setBirthday] = useState("");
+  const [birthday, setBirthday] = useState<Date | null>(null);
 
   function handleSubmit(event: React.FormEvent) {
     event.preventDefault();
@@ -14,36 +17,40 @@ export default function Form(): JSX.Element {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Stack spacing="20px">
-        <TextField
-          required
-          label="Name"
-          fullWidth
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <TextField
-          required
-          label="Email"
-          fullWidth
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <TextField
-          required
-          label="Birthday"
-          fullWidth
-          type="date"
-          InputLabelProps={{ shrink: true }}
-          value={birthday}
-          onChange={(e) => setBirthday(e.target.value)}
-        />
-        <Button variant="contained" type="submit" startIcon={<SaveOutlined />}>
-          Save
-        </Button>
-      </Stack>
-    </form>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <form onSubmit={handleSubmit}>
+        <Stack spacing="20px">
+          <TextField
+            required
+            label="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            required
+            label="Email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <DatePicker
+            label="Birthday"
+            value={birthday}
+            onChange={(newValue) => setBirthday(newValue)}
+            disableFuture
+            renderInput={(props) => (
+              <TextField {...props} required />
+            )}
+          />
+          <Button
+            variant="contained"
+            type="submit"
+            startIcon={<SaveOutlined />}
+          >
+            Save
+          </Button>
+        </Stack>
+      </form>
+    </LocalizationProvider>
   );
 }
