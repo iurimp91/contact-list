f642621e06374b3ec34161c47237f0d724ad0a9d
Iuri Magnago <iurimp91@gmail.com>
2021-12-06 10:58:27 -0300

    feat: add form submit button

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,11 +1,22 @@
-import { Stack, TextField } from "@mui/material";
+/* eslint-disable no-console */
+import { Stack, TextField, Button } from "@mui/material";
+import { SaveOutlined } from "@mui/icons-material";
 
-export default function Form() {
+export default function Form(): JSX.Element {
+  function handleSubmit() {
+    console.log("oi");
+  }
+
   return (
-    <Stack spacing="20px">
-      <TextField required label="Name" fullWidth />
-      <TextField required label="Email" fullWidth type="email" />
-      <TextField required label="Birthday" fullWidth type="date" InputLabelProps={{ shrink: true }} />
-    </Stack>
+    <form onSubmit={handleSubmit}>
+      <Stack spacing="20px">
+        <TextField required label="Name" fullWidth />
+        <TextField required label="Email" fullWidth type="email" />
+        <TextField required label="Birthday" fullWidth type="date" InputLabelProps={{ shrink: true }} />
+        <Button variant="contained" type="submit" startIcon={<SaveOutlined />}>
+          Save
+        </Button>
+      </Stack>
+    </form>
   );
 }
