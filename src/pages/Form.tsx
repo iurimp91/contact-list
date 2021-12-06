1f36152d946e14c925b5232500562d3f4903e721
Iuri Magnago <iurimp91@gmail.com>
2021-12-06 11:12:04 -0300

    refactor: add controlled input

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,18 +1,45 @@
 /* eslint-disable no-console */
 import { Stack, TextField, Button } from "@mui/material";
 import { SaveOutlined } from "@mui/icons-material";
+import React, { useState } from "react";
 
 export default function Form(): JSX.Element {
-  function handleSubmit() {
-    console.log("oi");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    console.log(name, email, birthday);
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <Stack spacing="20px">
-        <TextField required label="Name" fullWidth />
-        <TextField required label="Email" fullWidth type="email" />
-        <TextField required label="Birthday" fullWidth type="date" InputLabelProps={{ shrink: true }} />
+        <TextField
+          required
+          label="Name"
+          fullWidth
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <TextField
+          required
+          label="Email"
+          fullWidth
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          required
+          label="Birthday"
+          fullWidth
+          type="date"
+          InputLabelProps={{ shrink: true }}
+          value={birthday}
+          onChange={(e) => setBirthday(e.target.value)}
+        />
         <Button variant="contained" type="submit" startIcon={<SaveOutlined />}>
           Save
         </Button>
