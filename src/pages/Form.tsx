cae38f0807c43130f7bd1b9f451d02740c26e796
Iuri Magnago <iurimp91@gmail.com>
2021-12-07 19:42:47 -0300

    feat: add more form inputs

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,15 +1,21 @@
 /* eslint-disable no-console */
-import { Stack, TextField, Button } from "@mui/material";
+import { Stack, TextField, Button, TextFieldProps } from "@mui/material";
 import { SaveOutlined } from "@mui/icons-material";
 import { DatePicker, LocalizationProvider } from "@mui/lab";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
-
+import InputMask from "react-input-mask";
 import React, { useState } from "react";
 
 export default function Form(): JSX.Element {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
   const [birthday, setBirthday] = useState<Date | null>(null);
+  const [cep, setCep] = useState<string>("");
+  const [street, setStreet] = useState<string>("");
+  const [number, setNumber] = useState<string>("");
+  const [complement, setComplement] = useState<string>("");
+  const [city, setCity] = useState<string>("");
+  const [state, setState] = useState<string>("");
 
   function handleSubmit(event: React.FormEvent) {
     event.preventDefault();
@@ -38,9 +44,42 @@ export default function Form(): JSX.Element {
             value={birthday}
             onChange={(newValue) => setBirthday(newValue)}
             disableFuture
+            clearable
             renderInput={(props) => (
               <TextField {...props} required />
             )}
+          />
+          <InputMask mask="99999-999" value={cep} onChange={(e) => setCep(e.target.value)}>
+            {(props: JSX.IntrinsicAttributes & TextFieldProps) => <TextField label="CEP" required {...props} />}
+          </InputMask>
+          <TextField
+            required
+            label="Street"
+            value={street}
+            onChange={(e) => setStreet(e.target.value)}
+          />
+          <TextField
+            required
+            label="Number"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
+          />
+          <TextField
+            label="Complement"
+            value={complement}
+            onChange={(e) => setComplement(e.target.value)}
+          />
+          <TextField
+            required
+            label="City"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <TextField
+            required
+            label="State"
+            value={state}
+            onChange={(e) => setState(e.target.value)}
           />
           <Button
             variant="contained"
