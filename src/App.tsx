5fe8d1c6454651786a64008374be946b018b9de0
Iuri Magnago <iurimp91@gmail.com>
2021-12-10 11:41:35 -0300

    refactor: create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import { Routes, Route } from "react-router-dom";
-import { AppBar, Typography, Box, Container } from "@mui/material";
+import { AppBar, Typography, Container } from "@mui/material";
 import Home from "./pages/Home";
 import ContactPage from "./pages/ContactPage";
 import Form from "./pages/Form";
 
+function Header(): JSX.Element {
+  return (
+    <AppBar
+      position="fixed"
+      sx={{
+        backgroundColor: "#FFFFFF",
+      }}
+    >
+      <Typography
+        sx={{ color: "#000000", textAlign: "center", fontSize: "32px" }}
+      >
+        CONTACT LIST
+      </Typography>
+    </AppBar>
+  );
+}
+
 function App(): JSX.Element {
   return (
     <>
-      <AppBar
-        position="fixed"
-        sx={{
-          backgroundColor: "#FFFFFF",
-        }}
-      >
-        <Typography
-          sx={{ color: "#000000", textAlign: "center", fontSize: "32px" }}
-        >
-          CONTACT LIST
-        </Typography>
-      </AppBar>
+      <Header />
       <Container sx={{ backgroundColor: "#FAFAFA", pt: "68px", pb: "20px" }}>
         <Routes>
           <Route path="/" element={<Home />} />
