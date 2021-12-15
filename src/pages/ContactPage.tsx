1f2888dde89671dcb8540821845a6d987a03e33a
Iuri Magnago <iurimp91@gmail.com>
2021-12-15 15:33:37 -0300

    feat: style button on contact page

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -7,6 +7,7 @@ import {
   CakeOutlined,
   HomeOutlined,
   ModeEditOutline,
+  DeleteOutline
 } from "@mui/icons-material";
 import Contact from "../interfaces/Contact";
 import { getContactList, getContactByEmail, setContactList } from "../utils/localStorageHandlers";
@@ -45,13 +46,13 @@ export default function ContactPage(): JSX.Element {
     <Box height="calc(100vh - 88px)">
       <Box display="flex" alignItems="center" mb="15px">
         <PersonOutlineOutlined sx={{ fontSize: "40px", color: "#6AEFAB" }} />
-        <Typography sx={{ fontSize: "25px", color: "#000000", ml: "10px" }}>
+        <Typography sx={{ fontSize: "25px", color: "#000000", ml: "10px", wordBreak: "break-all" }}>
           {contact?.name}
         </Typography>
       </Box>
       <Box display="flex" alignItems="center" mb="15px">
         <EmailOutlined sx={{ fontSize: "40px", color: "#6AEFAB" }} />
-        <Typography sx={{ fontSize: "20px", color: "#000000", ml: "10px" }}>
+        <Typography sx={{ fontSize: "20px", color: "#000000", ml: "10px", wordBreak: "break-all" }}>
           {contact?.email}
         </Typography>
       </Box>
@@ -67,21 +68,23 @@ export default function ContactPage(): JSX.Element {
           ADDRESS
         </Typography>
       </Box>
-      <Typography sx={{ fontSize: "20px", color: "#000000", mt: "15px" }}>
+      <Typography sx={{ fontSize: "20px", color: "#000000", mt: "15px", wordBreak: "break-all" }}>
         {contact?.street}, {contact?.number}
       </Typography>
-      <Typography sx={{ fontSize: "20px", color: "#000000", mt: "15px" }}>
+      <Typography sx={{ fontSize: "20px", color: "#000000", mt: "15px", wordBreak: "break-all" }}>
         {contact?.cep}
       </Typography>
-      <Typography sx={{ fontSize: "20px", color: "#000000", mt: "15px" }}>
+      <Typography sx={{ fontSize: "20px", color: "#000000", mt: "15px", wordBreak: "break-all" }}>
         {contact?.city}/{contact?.state}
       </Typography>
-      <Typography sx={{ fontSize: "20px", color: "#000000", mt: "15px" }}>
+      <Typography sx={{ fontSize: "20px", color: "#000000", mt: "15px", wordBreak: "break-all" }}>
         Complement: {contact?.complement}
       </Typography>
       <Button
         fullWidth
-        variant="outlined"
+        color="secondary"
+        sx={{ mt: "20px" }}
+        variant="contained"
         startIcon={<ModeEditOutline />}
         onClick={() => navigate(`/form/${contact?.email}`)}
       >
@@ -89,8 +92,10 @@ export default function ContactPage(): JSX.Element {
       </Button>
       <Button
         fullWidth
+        color="secondary"
+        sx={{ mt: "20px" }}
         variant="outlined"
-        startIcon={<ModeEditOutline />}
+        startIcon={<DeleteOutline />}
         onClick={handleDelete}
       >
         DELETE
