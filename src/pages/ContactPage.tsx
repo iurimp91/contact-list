d06a3348393c5a62f131528b3e82f56b933edf1f
Iuri Magnago <iurimp91@gmail.com>
2021-12-16 10:59:42 -0300

    feat: contact page desktop layout

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Typography, Button, Container } from "@mui/material";
 import {
   PersonOutlineOutlined,
   EmailOutlined,
   CakeOutlined,
   HomeOutlined,
   ModeEditOutline,
-  DeleteOutline
+  DeleteOutline,
 } from "@mui/icons-material";
 import Contact from "../interfaces/Contact";
-import { getContactList, getContactByEmail, setContactList } from "../utils/localStorageHandlers";
+import {
+  getContactList,
+  getContactByEmail,
+  setContactList,
+} from "../utils/localStorageHandlers";
 import dayjs from "dayjs";
 
 export default function ContactPage(): JSX.Element {
@@ -19,7 +23,7 @@ export default function ContactPage(): JSX.Element {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if(!contactEmail) {
+    if (!contactEmail) {
       alert("Couldn't find the contact, please, try again.");
       return;
     }
@@ -44,63 +48,107 @@ export default function ContactPage(): JSX.Element {
   }
 
   return (
-    <Box height="calc(100vh - 88px)">
-      <Box display="flex" alignItems="center" mb="15px">
-        <PersonOutlineOutlined sx={{ fontSize: "40px", color: "#6AEFAB" }} />
-        <Typography sx={{ fontSize: "25px", color: "#000000", ml: "10px", wordBreak: "break-all" }}>
-          {contact?.name}
+    <Container maxWidth="sm">
+      <Box height="calc(100vh - 88px)">
+        <Box display="flex" alignItems="center" mb="15px">
+          <PersonOutlineOutlined sx={{ fontSize: "40px", color: "#6AEFAB" }} />
+          <Typography
+            sx={{
+              fontSize: "25px",
+              color: "#000000",
+              ml: "10px",
+              wordBreak: "break-all",
+            }}
+          >
+            {contact?.name}
+          </Typography>
+        </Box>
+        <Box display="flex" alignItems="center" mb="15px">
+          <EmailOutlined sx={{ fontSize: "40px", color: "#6AEFAB" }} />
+          <Typography
+            sx={{
+              fontSize: "20px",
+              color: "#000000",
+              ml: "10px",
+              wordBreak: "break-all",
+            }}
+          >
+            {contact?.email}
+          </Typography>
+        </Box>
+        <Box display="flex" alignItems="center" mb="15px">
+          <CakeOutlined sx={{ fontSize: "40px", color: "#6AEFAB" }} />
+          <Typography sx={{ fontSize: "20px", color: "#000000", ml: "10px" }}>
+            {dayjs(contact?.birthday).format("DD-MM-YYYY")}
+          </Typography>
+        </Box>
+        <Box display="flex" alignItems="center" mt="30px">
+          <HomeOutlined sx={{ fontSize: "40px", color: "#ED75EF" }} />
+          <Typography sx={{ fontSize: "25px", color: "#000000", ml: "10px" }}>
+            ADDRESS
+          </Typography>
+        </Box>
+        <Typography
+          sx={{
+            fontSize: "20px",
+            color: "#000000",
+            mt: "15px",
+            wordBreak: "break-all",
+          }}
+        >
+          {contact?.street}, {contact?.number}
         </Typography>
-      </Box>
-      <Box display="flex" alignItems="center" mb="15px">
-        <EmailOutlined sx={{ fontSize: "40px", color: "#6AEFAB" }} />
-        <Typography sx={{ fontSize: "20px", color: "#000000", ml: "10px", wordBreak: "break-all" }}>
-          {contact?.email}
+        <Typography
+          sx={{
+            fontSize: "20px",
+            color: "#000000",
+            mt: "15px",
+            wordBreak: "break-all",
+          }}
+        >
+          {contact?.cep}
         </Typography>
-      </Box>
-      <Box display="flex" alignItems="center" mb="15px">
-        <CakeOutlined sx={{ fontSize: "40px", color: "#6AEFAB" }} />
-        <Typography sx={{ fontSize: "20px", color: "#000000", ml: "10px" }}>
-          {dayjs(contact?.birthday).format("DD-MM-YYYY")}
+        <Typography
+          sx={{
+            fontSize: "20px",
+            color: "#000000",
+            mt: "15px",
+            wordBreak: "break-all",
+          }}
+        >
+          {contact?.city}/{contact?.state}
         </Typography>
-      </Box>
-      <Box display="flex" alignItems="center" mt="30px">
-        <HomeOutlined sx={{ fontSize: "40px", color: "#ED75EF" }} />
-        <Typography sx={{ fontSize: "25px", color: "#000000", ml: "10px" }}>
-          ADDRESS
+        <Typography
+          sx={{
+            fontSize: "20px",
+            color: "#000000",
+            mt: "15px",
+            wordBreak: "break-all",
+          }}
+        >
+          {contact?.complement && `Complement: ${contact?.complement}`}
         </Typography>
+        <Button
+          fullWidth
+          color="secondary"
+          sx={{ mt: "20px" }}
+          variant="contained"
+          startIcon={<ModeEditOutline />}
+          onClick={() => navigate(`/form/${contact?.email}`)}
+        >
+          EDIT
+        </Button>
+        <Button
+          fullWidth
+          color="secondary"
+          sx={{ mt: "20px" }}
+          variant="outlined"
+          startIcon={<DeleteOutline />}
+          onClick={handleDelete}
+        >
+          DELETE
+        </Button>
       </Box>
-      <Typography sx={{ fontSize: "20px", color: "#000000", mt: "15px", wordBreak: "break-all" }}>
-        {contact?.street}, {contact?.number}
-      </Typography>
-      <Typography sx={{ fontSize: "20px", color: "#000000", mt: "15px", wordBreak: "break-all" }}>
-        {contact?.cep}
-      </Typography>
-      <Typography sx={{ fontSize: "20px", color: "#000000", mt: "15px", wordBreak: "break-all" }}>
-        {contact?.city}/{contact?.state}
-      </Typography>
-      <Typography sx={{ fontSize: "20px", color: "#000000", mt: "15px", wordBreak: "break-all" }}>
-        {contact?.complement && `Complement: ${contact?.complement}`}
-      </Typography>
-      <Button
-        fullWidth
-        color="secondary"
-        sx={{ mt: "20px" }}
-        variant="contained"
-        startIcon={<ModeEditOutline />}
-        onClick={() => navigate(`/form/${contact?.email}`)}
-      >
-        EDIT
-      </Button>
-      <Button
-        fullWidth
-        color="secondary"
-        sx={{ mt: "20px" }}
-        variant="outlined"
-        startIcon={<DeleteOutline />}
-        onClick={handleDelete}
-      >
-        DELETE
-      </Button>
-    </Box>
+    </Container>
   );
 }
