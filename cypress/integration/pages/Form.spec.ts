479f26b7932fcc864710590861ef404bfebd2386
Iuri Magnago <iurimp91@gmail.com>
2022-01-20 14:53:43 -0300

    test: add email input tests

--- cypress/integration/pages/Form.spec.ts
+++ cypress/integration/pages/Form.spec.ts
@@ -22,6 +22,36 @@ describe("Form tests", () => {
 
     it("accepts strings as its value", () => {
       cy.get("#name-input").type("Test").should("have.value", "Test");
+
+      cy.get("button[type=submit]").click();
+
+      cy.get("label").contains("NAME").parent().find("p").should("not.exist");
+    });
+  });
+
+  context("email input", () => {
+    it("gets error status when form is submitted with it empty", () => {
+      cy.get("button[type=submit]").click();
+
+      cy.get("#email-input").next().should("have.css", "border-color").and("equal", "rgb(211, 47, 47)");
+    
+      cy.get("label").contains("EMAIL").parent().should("contain", "Please, this field must be filled.");
+    });
+
+    it("keeps error status if the value isn't a valid email", () => {
+      cy.get("button[type=submit]").click();
+
+      cy.get("#email-input").type("test").next().should("have.css", "border-color").and("equal", "rgb(211, 47, 47)");
+    
+      cy.get("label").contains("EMAIL").parent().should("contain", "Please, enter a valid email format.");
+    });
+
+    it("accepts emails strings as its value", () => {
+      cy.get("#email-input").type("test@test.com").should("have.value", "test@test.com");
+
+      cy.get("button[type=submit]").click();
+
+      cy.get("label").contains("EMAIL").parent().find("p").should("not.exist");
     });
   });
 });
